d70e75f5d984d8862bdcddfaed2cb93e60683c28
aksoy <aksoy.android@gmail.com>
2020-05-18 23:11:03 +0300

    Proje dosyası ekle.

---
+++ ObservableCollectionOrnek/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using ObservableCollectionOrnek.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ObservableCollectionOrnek
+{
+    /// <summary>
+    /// MainWindow.xaml etkileşim mantığı
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static ObservableCollection<Person> PeopleDataSource { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            PeopleDataSource = new ObservableCollection<Person>(Person.getPeople());
+            this.DataContext = this;
+
+        }
+
+        private void BtnAddPerson_Click(object sender, RoutedEventArgs e)
+        {
+            PeopleDataSource.Add(new Person { personID = PeopleDataSource.Count + 1, Name = txtName.Text, Title = txtTitle.Text });
+        }
+        
+    }
+}
---
+++ ObservableCollectionOrnek/Model/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObservableCollectionOrnek.Model
+{
+    public class Person
+    {
+        public int personID { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+
+        public static IEnumerable<Person> getPeople()
+        {
+            ObservableCollection<Person> People = new ObservableCollection<Person>();
+            People.Add(new Person { personID = 1, Name = "Hasan AKSOY", Title = "Senior Software Developer" });
+
+            return People;
+        }
+
+    }
+}
